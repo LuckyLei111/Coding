3cecdfa425d20dd4a5907afe80443ac87fafc7a5
Lucky <luckylei110@qq.com>
2017-11-23 18:59:05 +0800

    \

--- project/HTML5-Puzzle/js/game.js
+++ project/HTML5-Puzzle/js/game.js
@@ -8,12 +8,11 @@ var d_mark = new Array(
     [2,6],
     [1,5,7],
     [2,4,6,8],
-    [3,5,9]
+    [3,5,9],
     [4,8],
     [5,7,9],
     [6,8]
 );
-
 //保存div可移动的实际位置
 var g_divXY = new Array(
     [0],
@@ -40,17 +39,39 @@ function move(id){
         break;
     }
     //for循环用于找出div的位置
-    alert(i);
-    var sd_target = 0;  //保存小div的移动的位置,0表示不能移动
+    console.log('点击的'+i);
+    var sd_target = 0;  //保存小div的移动的位置,0表示不能移动		//i = 8
     sd_target = whereDiv(i)
     //whereDiv函数用于找出被点击的div可以去的位置，如果返回0，表示不能移动;
-    alert(d_mark[1]);
+    console.log('最后得到'+sd_target);
+    if(sd_target != 0){
+    	console.log("可移动");
+    	
+    	d[i] = 0;
+    	d[g_divXY] = id;
+    	document.querySelector('.a1')
+    }else{
+    	console.log('不可移动');
+    }
 };
 
 function whereDiv(val){
-    //判断是否可以移动函数,参数是被点击的div的编号
+    //判断是否可以移动函数,参数是被点击的div的编号;		
+    //alert('hello'+d_mark[val].length);
     var j = 0;
-    var move_flog = false;
-    for(j = 0;j<d_mark[val];++j){
+    var move_flog = false;  //true时可以动,false时，不可移动;	
+    for(j = 0; j < d_mark[val].length; ++j){
+    	console.log('分别循环的值是'+d[d_mark[val][j]]);
+    	//8的length =3
+        if(d[d_mark[val][j]] == 0){
+            move_flog = true;
+            break;
+            //如果点击的div有0时,说明div可以移动的，则跳出循环。
+        }
+    }
+    if(move_flog == true){
+        return d[d_mark[val][j]];
+    }else{
+        return 0;
     }
 }
