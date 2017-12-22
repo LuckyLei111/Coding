a565e25c6fe7472ebc737eb09f925587a972b874
yelei <luckylei110@qq.com>
2017-12-22 13:06:41 +0800

    \

--- project/douban-movie/js/douban.js
+++ project/douban-movie/js/douban.js
@@ -9,11 +9,24 @@ $.ajax({
     type:'GET',
     data:{
         start:0,
-        count:20
+        count:250
     },
     dataType: 'jsonp'
 }).done(function (ret) {
-    console.log(ret)
+    setDate(ret);
 }).fail(function() {
     console.log('Error')   
 })
+function setDate(ret) {
+    var moData = ret.subjects;
+    for (var i = 0; i < moData.length; i++) {
+        console.log(moData[i].alt)
+        console.log(moData[i].title);
+        console.log(moData[i].genres);
+        console.log(moData[i].casts)
+        console.log(moData[i].images.small);
+        console.log(moData[i].rating.average);
+        console.log(moData[i].collect_count)
+        console.log(moData[i].directors)
+    }
+}
