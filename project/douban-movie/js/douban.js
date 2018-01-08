eceee981c4951ad9e749a539ebaa476d14ccb47b
yelei <luckylei110@qq.com>
2018-01-08 17:47:45 +0800

    \

--- project/douban-movie/js/douban.js
+++ project/douban-movie/js/douban.js
@@ -20,33 +20,30 @@ $.ajax({
     console.log('Error')   
 });
 
-// function setDate(ret) {
-//     var moData = ret.subjects;
-//     var mloading = "";
-//     mloading += "<div class=\"loading\"><span class=\"iconfont icon-loading\"><\/span><\/div>\n";
-//     for (var a in moData) {
-//         var strVar = "";
-//         strVar += "<div class=\"item\">\n";
-//         strVar += "                <a href=\"" + moData[a].alt+"\">\n";
-//         strVar += "                    <div class=\"cover\"><img src=\"" + moData[a].images.large +"\" alt=\"\"><\/div>\n";
-//         strVar += "                    <div class=\"detail\">\n";
-//         strVar += "                        <h2>" + moData[a].title+"<\/h2>\n";
-//         strVar += "                        <div><span>" + moData[a].rating.average+"<\/span>评分/<span>88888<\/span>收藏<\/div>\n";
-//         strVar += "                        <div><span class=\"year\">" + moData[a].year + "<\/span>/<span class=\"type\">" + moData[a].genres[0]+moData[a].genres[1] +"<\/span><\/div>\n";
-//         strVar += "                        <div>导演：<span>宫崎骏<\/span><\/div>\n";
-//         strVar += "                        <div>主演：<span>柊瑠美、入野自由、夏木真理<\/span><\/div>\n";
-//         strVar += "                    <\/div>\n";
-//         strVar += "                <\/a>\n";
-//         strVar += "             <\/div>\n";
-//         var mloading = "";
-//         mloading += "<div class=\"loading\"><span class=\"iconfont icon-loading\"><\/span><\/div>\n";
-//         $('#top250').append(strVar);
-//     }
-//     $('#top250').append(mloading);
-// }
-
-function setDate(ret){
-    data.subjects.array.forEach(element => {
-        
-    });
+function setDate(data) {
+    var moData = data.subjects;
+    var mloading = "";
+    mloading += "<div class=\"loading\"><span class=\"iconfont icon-loading\"><\/span><\/div>\n";
+    for (var a in moData) {
+        var strVar = "";
+        strVar += "<div class=\"item\">\n";
+        strVar += "                <a href=\"" + moData[a].alt+"\">\n";
+        strVar += "                    <div class=\"cover\"><img src=\"" + moData[a].images.large +"\" alt=\"\"><\/div>\n";
+        strVar += "                    <div class=\"detail\">\n";
+        strVar += "                        <h2>" + moData[a].title+"<\/h2>\n";
+        strVar += "                        <div><span>" + moData[a].rating.average + "<\/span>评分/<span>" + moData[a].collect_count +"<\/span>收藏<\/div>\n";
+        strVar += "                        <div><span class=\"year\">" + moData[a].year + "<\/span>/<span class=\"type\">" + moData[a].genres[0]+moData[a].genres[1] +"<\/span><\/div>\n";
+        strVar += "                        <div>导演：<span>" + moData[a].directors[0].name +"<\/span><\/div>\n";
+        strVar += "                        <div>主演：<span>柊瑠美、入野自由、夏木真理<\/span><\/div>\n";
+        strVar += "                    <\/div>\n";
+        strVar += "                <\/a>\n";
+        strVar += "             <\/div>\n";
+        $('#top250').append(strVar);
+    }
+    $('#top250').append(mloading);
+}
+onClickTop();
+function onClickTop(){
+    var scorllTop = $('#top250').scorllTop;
+    console.log(scorllTop);
 }
