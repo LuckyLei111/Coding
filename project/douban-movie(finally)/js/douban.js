d491679a7a75d1bf73054d6ad42774ba5a008fb0
yelei <luckylei110@qq.com>
2018-01-10 18:03:44 +0800

    \

---
+++ project/douban-movie(finally)/js/douban.js
@@ -0,0 +1,63 @@
+var top250 = {
+    init:function(){
+        console.log('top250 ok');
+        this.$element = $('#top250');
+        this._index = 0
+        this.bind();
+        //初始化
+    },
+    bind:function(){
+        console.log('bind ok')
+        this.start();
+        //事件
+    },
+    start:function(callback){
+        console.log('start ok');
+        $.ajax({
+            url: 'http://api.douban.com/v2/movie/top250',
+            type:'GET',
+            data:{
+                count: this._index || 0
+            },
+            dataType:'jsonp'
+        }).done(function(ret){
+            callback(ret);
+        }).fail(function(){
+            console.log('Err')
+        })
+    },
+    render:function(){
+        //打印数据
+    }
+}
+
+var beimei = {
+    init:function(){
+        console.log('beimei ok');
+    }
+}
+var ssuo = {
+    init:function(){
+        console.log('ssuo ok');
+    }
+}
+
+var app = {
+    init:function(){
+        this.tabs = $('footer div');
+        this.panel = $('main section');
+        this.bind();
+
+        top250.init();
+        beimei.init();
+        ssuo.init();
+    },
+    bind:function(){
+        var _this = this;
+        this.tabs.on('click',function(){
+            $(this).addClass('active').siblings().removeClass('active');
+            _this.panel.eq($(this).index()).fadeIn().siblings().hide();
+        })
+    }
+}
+app.init();
--- project/douban-movie/js/douban.js
+++ project/douban-movie/js/douban.js
@@ -5,45 +5,35 @@ $('footer div').click(function () {
 })
 //tab切换
 
-$.ajax({
-    url: 'http://api.douban.com/v2/movie/top250',
-    type: 'GET',
-    data: {
-        start: 0,
-        count: 20
-    },
-    dataType: 'jsonp'
-}).done(function (ret) {
-    console.log(ret);
-    setDate(ret);
-}).fail(function () {
-    console.log('Error')
-});
-// function setDate(ret) {
-//     var moData = ret.subjects;
-//     var mloading = "";
-//     mloading += "<div class=\"loading\"><span class=\"iconfont icon-loading\"><\/span><\/div>\n";
-//     for (var a in moData) {
-//         var strVar = "";
-//         strVar += "<div class=\"item\">\n";
-//         strVar += "                <a href=\"" + moData[a].alt+"\">\n";
-//         strVar += "                    <div class=\"cover\"><img src=\"" + moData[a].images.large +"\" alt=\"\"><\/div>\n";
-//         strVar += "                    <div class=\"detail\">\n";
-//         strVar += "                        <h2>" + moData[a].title+"<\/h2>\n";
-//         strVar += "                        <div><span>" + moData[a].rating.average+"<\/span>评分/<span>88888<\/span>收藏<\/div>\n";
-//         strVar += "                        <div><span class=\"year\">" + moData[a].year + "<\/span>/<span class=\"type\">" + moData[a].genres[0]+moData[a].genres[1] +"<\/span><\/div>\n";
-//         strVar += "                        <div>导演：<span>宫崎骏<\/span><\/div>\n";
-//         strVar += "                        <div>主演：<span>柊瑠美、入野自由、夏木真理<\/span><\/div>\n";
-//         strVar += "                    <\/div>\n";
-//         strVar += "                <\/a>\n";
-//         strVar += "             <\/div>\n";
-//         var mloading = "";
-//         mloading += "<div class=\"loading\"><span class=\"iconfont icon-loading\"><\/span><\/div>\n";
-//         $('#top250').append(strVar);
-//     }
-//     $('#top250').append(mloading);
-// }
 
+var index = 0;
+
+var contrent = false;//枷锁机制
+
+
+start();
+function start(){
+
+    if(contrent)return
+    contrent = true;
+    $.ajax({
+        url: 'http://api.douban.com/v2/movie/top250',
+        type: 'GET',
+        data: {
+            start: index,
+            count: 10
+        },
+        dataType: 'jsonp'
+    }).done(function (ret) {
+        console.log(ret);
+        setDate(ret);
+        index+=10;
+    }).fail(function () {
+        console.log('Error')
+    }).always(function(){
+        contrent = false;
+    });
+}
 function setDate(data) {
     data.subjects.forEach(function (movie) {
         var tpl = '<div class="item">\
@@ -86,14 +76,19 @@ function setDate(data) {
             return StarringArr.join('、');
         })
         $('#top250 .loading').before($node);
+        if (index == '250') {
+            alert('到底了');
+        }
     });
 }
+
 $('section').on('scroll',function(){
     var loadTop = $('.loading').offset().top;
     var winHeight = $('section').height();
     var loadHeight = $('.loading').height();
     if(loadTop < winHeight + loadHeight){
         console.log('true');
+        start();
     }else{
         console.log('false');
     }
