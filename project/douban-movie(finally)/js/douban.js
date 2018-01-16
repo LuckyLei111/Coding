66c4203a37bf8633b06da0e6047d97b4d6a291b1
yelei <luckylei110@qq.com>
2018-01-16 11:33:43 +0800

    \

--- project/douban-movie(finally)/js/douban.js
+++ project/douban-movie(finally)/js/douban.js
@@ -1,33 +1,101 @@
+var dateItem = {
+    isLoading: function ($viewport, $content){
+        return $viewport.height() + $viewport.scrollTop() + 10 > $content.height()
+    },
+    createNode:function(movie){
+        var tpl = '<div class="item">\
+            <a href = "#" >\
+                <div class="cover"><img src="http://img3.doubanio.com/view/photo/s_ratio_poster/public/p1910830216.jpg" alt=""></div>\
+                    <div class="detail">\
+                        <h2>千里千寻</h2>\
+                        <div><span class="score"></span>评分/<span class="collect"></span></div>\
+                        <div><span class="year">2001</span>/<span class="type"></span></div>\
+                        <div>导演：<span class="director"></span></div>\
+                        <div>主演：<span class="Starring">柊瑠美、入野自由、夏木真理</span></div>\
+                    </div>\
+                </a>\
+            </div >';
+        var $node = $(tpl);
+        $node.find('.cover img').attr('src', movie.images.medium);
+        $node.find('.detail h2').text(movie.title);
+        $node.find('.detail .score').text(movie.rating.average);
+        $node.find('.detail .collect').text(movie.collect_count);
+        $node.find('.detail .year').text(movie.year);
+        $node.find('.detail .type').text(function () {
+            var movieTypeArr = [];
+            movie.genres.forEach(function (item) {
+                movieTypeArr.push(item);
+            });
+            return movieTypeArr.join('、');
+        });
+        $node.find('.detail .director').text(function () {
+            var directorArr = [];
+            movie.directors.forEach(function (item) {
+                directorArr.push(item.name);
+            })
+            return directorArr.join('、');
+        });
+        $node.find('.detail .Starring').text(function () {
+            var StarringArr = [];
+            movie.casts.forEach(function (item) {
+                StarringArr.push(item.name);
+            })
+            return StarringArr.join('、');
+        });
+        return $node;
+    }
+}
+
 var top250 = {
     init:function(){
         console.log('top250 ok');
         this.$element = $('#top250');
-        this._index = 20
+        this.$cont = $('#top250 .cont');
+        this.index = 0
+        this.isFinish = false;
         this.bind();
         //初始化
     },
     bind:function(){
-        console.log('bind ok')
-        this.start();
-        //事件
+        console.log('bind ok');
+        var _this = this;
+        this.$element.scroll(function(){
+            if(!_this.isFinish && dateItem.isLoading(_this.$element,_this.$cont)){
+                _this.start();
+            }
+        })
     },
-    start:function(callback){
-        console.log('start ok');
+    start: function () {
+        var _this = this
+        this.getData(function (data) {
+            _this.render(data)
+        })
+    },
+    getData:function(callback){
+        console.log('getDate ok');
+        var _this = this;
         $.ajax({
             url: 'http://api.douban.com/v2/movie/top250',
             type:'GET',
             data:{
-                count: this._index || 0
+                count: _this.index || 0
             },
             dataType:'jsonp'
         }).done(function(ret){
-            console.log(ret)
+            _this.index+=10;
+            if(_this.index >=ret.total){
+                _this.isFinish = true;
+            }
+            callback && callback(ret);
         }).fail(function(){
             console.log('Err')
         })
     },
-    render:function(){
-        //打印数据
+    render:function(data){
+        var _this = this;
+        data.subjects.forEach(function(movie){
+            _this.$cont.append(dateItem.createNode(movie));
+        });
     }
 }
 
@@ -60,5 +128,4 @@ var app = {
         })
     }
 }
-
 app.init();
